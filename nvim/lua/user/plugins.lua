8fb0e8001370f76fb408c21a36f3d852e7d8bc2c
Liam Moore <liamail4@gmail.com>
2022-05-01 12:02:29 +0100

    feat(nvim): file finder setup (telescope)
    add native fzf support

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -69,6 +69,14 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
+  -- Telescope
+  use {
+  	'nvim-telescope/telescope.nvim',
+  	requires = {
+		{'nvim-lua/plenary.nvim'},
+		{'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+	}
+  }
 
 
   -- Automatically set up your configuration after cloning packer.nvim
