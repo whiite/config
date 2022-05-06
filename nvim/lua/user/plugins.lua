b0b9dc48f8b2f93e108dbd6e3bbfd32c46802585
White <liamail4@gmail.com>
2022-05-06 14:50:28 +0100

    feat(nvim): specs - cursor animations after jumps
    Helps find the cursor when moving around

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -84,6 +84,7 @@ return packer.startup(function(use)
   -- Prettier UI
   use "lukas-reineke/indent-blankline.nvim" -- Indent guides and invisible character support
   use "norcalli/nvim-colorizer.lua"
+  use {'edluffy/specs.nvim'}
 
   -- cmp plugins
   use {
