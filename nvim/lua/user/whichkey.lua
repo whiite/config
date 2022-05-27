668ece1d55a5e4833d722fc97483ed9c899db130
Liam Moore <liamail4@gmail.com>
2022-05-27 17:29:36 +0100

    feat(nvim): add deno repl terminal & better toggle
    Ability to toggle all terminals (hidden = false)
    Removed unused terminals

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -189,8 +189,7 @@ local mappings = {
 	t = {
 		name = "Terminal",
 		n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-		u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-		t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+		d = { "<cmd>lua _DENO_TOGGLE()<cr>", "Deno" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
