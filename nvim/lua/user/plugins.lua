b81a2f0c0429d30fdeddb033e02bfaa1704778e4
Liam Moore <liamail4@gmail.com>
2022-05-29 12:38:37 +0100

    refactor(nvim): use lua API for packer updates

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -15,13 +15,14 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- Auto command that reloads neovim whenever you save the plugins.lua file
+local auto_group = "packer_user_config"
+vim.api.nvim_create_augroup(auto_group, { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = auto_group,
+	pattern = { "plugins.lua" },
+	command = "source <afile> | PackerSync",
+})
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
