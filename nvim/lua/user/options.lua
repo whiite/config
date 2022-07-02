3175386d044e15f62081b35fe2752e2c96da6271
Liam Moore <liamail4@gmail.com>
2022-07-02 14:57:22 +0100

    feat(nvim): use faster filetype detection

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -35,6 +35,9 @@ local options = {
 	sidescrolloff = 8,
 	guifont = "monospace:h17", -- the font used in graphical neovim applications
 }
+-- Faster filetype detection (will become the default in future versions)
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
 
 vim.opt.shortmess:append("c")
 
