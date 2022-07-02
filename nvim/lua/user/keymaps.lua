cb0d861e49e597387ed5ee25694bd5748f010ab7
Liam Moore <liamail4@gmail.com>
2022-07-02 14:57:07 +0100

    feat(nvim): use "alt-j" and "alt-k" to move lines

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -30,6 +30,8 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
@@ -71,4 +73,3 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
