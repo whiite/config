3a9b0c161053fb5d0dfb81916a1b4447a15ff772
Liam Moore <liamail4@gmail.com>
2022-08-02 20:40:45 +0100

    feat(nvim): use tokyonight colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,4 +1,8 @@
-local colorscheme = "codemonkey"
+local colorscheme = "tokyonight"
+vim.g.tokyonight_style = "storm" -- "storm" | "night" | "day"
+vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_dark_sidebar = true
+vim.g.tokyonight_transparent_sidebar = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
