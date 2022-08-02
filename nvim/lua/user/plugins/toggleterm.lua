2b4c2f190c569fbe024f2b6c867c17d85353d1cc
Liam Moore <liamail4@gmail.com>
2022-08-02 20:54:00 +0100

    feat(nvim): terminal Ctrl-C to insert mode and cancel command

--- nvim/lua/user/plugins/toggleterm.lua
+++ nvim/lua/user/plugins/toggleterm.lua
@@ -31,6 +31,7 @@ function _G.set_terminal_keymaps()
 	local opts = { noremap = true }
 	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-[>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "n", "<C-c>", [[<C-\><C-n>i<C-c>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
