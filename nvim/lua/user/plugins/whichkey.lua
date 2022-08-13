e967d3ea00b4a461501c26d18794d517b877c2bc
Liam Moore <liamail4@gmail.com>
2022-08-13 10:19:14 +0100

    feat(nvim): add terminal shortcut for lazygit

--- nvim/lua/user/plugins/whichkey.lua
+++ nvim/lua/user/plugins/whichkey.lua
@@ -215,6 +215,7 @@ local mappings = {
 		n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
 		d = { "<cmd>lua _DENO_TOGGLE()<cr>", "Deno" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+		g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
