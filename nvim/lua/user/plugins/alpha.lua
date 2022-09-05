b2ea37dd1d5b24aaf278fcd472ae4b08351ddd76
Liam Moore <liamail4@gmail.com>
2022-09-05 17:39:13 +0100

    refactor(nvim): simpler more useful dashboard

--- nvim/lua/user/plugins/alpha.lua
+++ nvim/lua/user/plugins/alpha.lua
@@ -37,32 +37,50 @@ local neovim_text = {
 }
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = aperture
+
+dashboard.section.header.val = neovim_text
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("P", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("T", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
-	-- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
-	return "whiite"
+	local total_plugins = #vim.tbl_keys(packer_plugins)
+	local datetime = os.date(" %d-%m-%Y")
+	local version = vim.version()
+	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+	return " " .. total_plugins .. " plugins" .. nvim_version_info
 end
 
 dashboard.section.footer.val = footer()
-
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
+
+local header_button_padding = 4
+local occu_height = #dashboard.section.header.val + 2 * #dashboard.section.buttons.val + header_button_padding
+local header_padding = math.max(0, math.ceil((vim.fn.winheight("$") - occu_height) * 0.25))
+local footer_button_padding_ub = vim.o.lines - header_padding - occu_height - #dashboard.section.footer.val - 3
+local footer_button_padding = math.floor((vim.fn.winheight("$") - 2 * header_padding - occu_height))
+footer_button_padding = math.max(
+	0,
+	math.max(math.min(0, footer_button_padding), math.min(math.max(0, footer_button_padding), footer_button_padding_ub))
+)
+dashboard.config.layout = {
+	{ type = "padding", val = header_padding },
+	dashboard.section.header,
+	{ type = "padding", val = header_button_padding },
+	dashboard.section.buttons,
+	{ type = "padding", val = footer_button_padding },
+	dashboard.section.footer,
+}
+
 alpha.setup(dashboard.opts)
