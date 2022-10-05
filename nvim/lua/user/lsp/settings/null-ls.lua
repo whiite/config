d9a9c06646af69d5178e9540ee9222568904a76b
Liam Moore <liamail4@gmail.com>
2022-10-05 18:35:24 +0100

    feat(nvim): add CSpell code actions

--- nvim/lua/user/lsp/settings/null-ls.lua
+++ nvim/lua/user/lsp/settings/null-ls.lua
@@ -11,6 +11,7 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 
+-- baaad name
 function M.setup(on_attach)
 	null_ls.setup({
 		debug = false,
@@ -37,6 +38,7 @@ function M.setup(on_attach)
 
 			-- Actions
 			actions.gitsigns,
+			actions.cspell,
 		},
 		on_attach = on_attach,
 	})
