2bc7fa1f145f5af261d628526796f6446d6c2ff7
White <liamail4@gmail.com>
2022-11-15 15:17:05 +0000

    fix(nvim): prefer denols for single files over tsserver

--- nvim/lua/user/lsp/settings/tsserver.lua
+++ nvim/lua/user/lsp/settings/tsserver.lua
@@ -1,8 +1,3 @@
-local status_ok, lspconfig = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
 return {
-	root_dir = lspconfig.util.root_pattern("package.json"),
+	single_file_support = false,
 }
