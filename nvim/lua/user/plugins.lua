db3d9d33063d9e4b51ad6b5858e5db03b1bc4bb1
Liam Moore <liamail4@gmail.com>
2022-11-30 08:02:03 +0000

    fix(nvim): treesitter starting on first load of nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -252,7 +252,10 @@ return packer.startup(function(use)
 	-- Treesitter
 	use({
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
 		requires = {
 			-- extensions
 			{
