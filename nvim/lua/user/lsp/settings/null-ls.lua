d9c64074fc5bf95c028200abfeedf85eb6466454
Liam Moore <liamail4@gmail.com>
2023-01-05 08:13:57 +0000

    style(nvim): minor comment tidy for null-ls

--- nvim/lua/user/lsp/settings/null-ls.lua
+++ nvim/lua/user/lsp/settings/null-ls.lua
@@ -9,9 +9,9 @@ local M = {}
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local actions = null_ls.builtins.code_actions
 
--- baaad name
 function M.setup(on_attach)
 	null_ls.setup({
 		debug = false,
