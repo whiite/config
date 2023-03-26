ad0366c07ede9887f8d309cd5b275da09c14d4c9
Liam Moore <liamail4@gmail.com>
2023-03-26 15:33:12 +0100

    refactor(nvim): migrate nvim-tree to on_attach

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -20,8 +20,25 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+end
+
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-nvim_tree.setup({ -- BEGIN_DEFAULT_OPTS
+nvim_tree.setup({
+	on_attach = on_attach,
 	auto_reload_on_write = true,
 	disable_netrw = true,
 	hijack_netrw = true,
@@ -42,19 +59,11 @@ nvim_tree.setup({ -- BEGIN_DEFAULT_OPTS
 		relativenumber = false,
 		side = "left",
 		signcolumn = "yes",
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-			},
-		},
 	},
 	renderer = {
 		add_trailing = true,
-		highlight_git = true, -- Highlight file names with git changes
-		group_empty = true, -- Compact folders that only contain a single folder into one node in the file tree
+		highlight_git = true,     -- Highlight file names with git changes
+		group_empty = true,       -- Compact folders that only contain a single folder into one node in the file tree
 		highlight_opened_files = "all", -- Folder and file icon highlight for opened files/directories
 		indent_markers = {
 			enable = true,
