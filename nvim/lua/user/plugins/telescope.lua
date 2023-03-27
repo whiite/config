b55b2aa4b9ea9c6fd0ddc063f855cc1905b48f4f
Liam Moore <liamail4@gmail.com>
2023-03-27 20:47:38 +0100

    fix(nvim): don't split on every go to definition

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -88,12 +88,10 @@ telescope.setup({
 		},
 		lsp_references = {
 			theme = "cursor",
-			jump_type = "split",
 			show_line = false,
 		},
 		lsp_definitions = {
 			theme = "ivy",
-			jump_type = "split",
 			show_line = false,
 		},
 	},
@@ -104,7 +102,7 @@ telescope.setup({
 		-- }
 		-- please take a look at the readme of the extension you want to configure
 		fzf = {
-			fuzzy = true, -- false will only do exact matching
+			fuzzy = true,          -- false will only do exact matching
 			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
