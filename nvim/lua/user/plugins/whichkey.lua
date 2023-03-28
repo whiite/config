9d023339e1bc981f4f00bd4d7935c15dfe8e992c
Liam Moore <liamail4@gmail.com>
2023-03-28 21:12:55 +0100

    refactor(nvim): remove custom comment toggle binding

--- nvim/lua/user/plugins/whichkey.lua
+++ nvim/lua/user/plugins/whichkey.lua
@@ -79,7 +79,6 @@ local opts = {
 }
 
 local mappings = {
-	["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<cr>', "Comment" },
 	["rn"] = { '<cmd>lua require("renamer").rename()<cr>', "Rename symbol" },
 	["a"] = { "<cmd>Alpha<cr>", "Alpha" },
 	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
@@ -216,18 +215,6 @@ local mappings = {
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
 	},
-}
-
-local vopts = {
-	mode = "v", -- VISUAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
-local vmappings = {
-	["/"] = { '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', "Comment" },
 }
 
 which_key.setup(setup)
