09b3826c8e7c8de1f417b0eaa75b580e1bbff4e3
Liam Moore <liamail4@gmail.com>
2023-05-05 08:32:07 +0100

    fix(nvim): alpha icons for nerd font v3

--- nvim/lua/user/plugins/alpha.lua
+++ nvim/lua/user/plugins/alpha.lua
@@ -40,22 +40,22 @@ local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = neovim_text
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("f", "   Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("P", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("T", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("r", "󰈢  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("T", "󰊄  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("q", "󰗼  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
 	local total_plugins = require("lazy").stats().count
 	local datetime = os.date(" %d-%m-%Y")
 	local version = vim.version()
-	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-	return " " .. total_plugins .. " plugins" .. nvim_version_info
+	return " " .. total_plugins .. " plugins" .. nvim_version_info
 end
 
 dashboard.section.footer.val = footer()
