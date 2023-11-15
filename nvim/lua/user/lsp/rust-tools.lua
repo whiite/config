097217681d501cf9dfe118eddb1b5d63d8a14a4b
White <liamail4@gmail.com>
2023-11-15 20:38:02 +0000

    feat(nvim): use leptosfmt for formatting rust

--- nvim/lua/user/lsp/rust-tools.lua
+++ nvim/lua/user/lsp/rust-tools.lua
@@ -14,6 +14,21 @@ rust_tools.setup({
 	server = {
 		on_attach = lsp_handlers.on_attach,
 		capabilities = lsp_handlers.capabilities,
+		settings = {
+			["rust-analyzer"] = {
+				rustfmt = {
+					overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+				},
+				-- procMacro = {
+				-- 	ignored = {
+				-- 		leptos_macro = {
+				-- 			"component",
+				-- 			"server",
+				-- 		},
+				-- 	},
+				-- },
+			},
+		},
 	},
 	dap = {
 		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
