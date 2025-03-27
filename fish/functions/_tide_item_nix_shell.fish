77840786d45e076f27257a4d690f9c940b3c07e7
Liam <liam@Eldin.local>
2025-03-27 09:57:14 +0000

    refactor(fish): simplify nix_shell tide item for use with the two line prompt

--- fish/functions/_tide_item_nix_shell.fish
+++ fish/functions/_tide_item_nix_shell.fish
@@ -1,5 +1,5 @@
 function _tide_item_nix_shell
     if test -n "$IN_NIX_SHELL" || echo "$PATH" | grep -qc /nix/store/
-        _tide_print_item nix_shell $tide_nix_shell_icon' ' "nix shell"
+        _tide_print_item nix_shell "nix shell $tide_nix_shell_icon "
     end
 end
