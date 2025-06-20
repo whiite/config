28e7749f587f1648adc6fbf8e43489b6bae3c8e2
Liam Moore <liamail4@gmail.com>
2025-06-20 14:56:18 +0100

    feat(fish): switch to vi bindings
    Keeping most of the default bindings too with the default ctrl-n bind

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,19 @@
+# Vi mode config
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+    fish_default_key_bindings -M default
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+    bind --mode insert ctrl-n down-or-search # Use default binding 
+    bind --mode default ctrl-n down-or-search
+end
+set fish_cursor_insert block blink
+
 zoxide init fish | source
 fzf --fish | source
 era completion fish | source
