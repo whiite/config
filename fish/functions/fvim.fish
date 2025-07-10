d0a2b9c78314be5d8420cd927ffb069723da60c1
Liam Moore <liamail4@gmail.com>
2025-07-10 08:13:11 +0100

    feat(fish): auto select file for fvim command when there is only one match

--- fish/functions/fvim.fish
+++ fish/functions/fvim.fish
@@ -1,6 +1,6 @@
 function fvim --argument-names query --description 'Fuzzy find file and open in nvim'
     set -lx FZF_DEFAULT_COMMAND 'rg --files --hidden'
-    set --local filePath (fzf -q "$argv")
+    set --local filePath (fzf -q "$argv" -1 )
     if test -z $filePath
         return 1
     end
