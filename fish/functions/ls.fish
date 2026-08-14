9a57364aae27ae5b8888344f62bce6f6d4e751c8
Liam Moore <liamail4@gmail.com>
2026-08-14 08:27:25 +0100

    fix(fish): eza alias (ls) usage

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza --icons' --description 'alias ls=eza --icons'
-    eza --icons $argv
+function ls --wraps='eza --icons always' --description 'alias ls=eza --icons always'
+    eza --icons always $argv
 end
