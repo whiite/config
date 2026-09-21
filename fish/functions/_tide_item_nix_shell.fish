595120ed6f91c536db0fb12be6814367501b8042
Liam Moore <liamail4@gmail.com>
2026-09-21 16:56:34 +0100

    build(fish): update fisher and tide

--- fish/functions/_tide_item_nix_shell.fish
+++ fish/functions/_tide_item_nix_shell.fish
@@ -1,5 +1,3 @@
 function _tide_item_nix_shell
-    if test -n "$IN_NIX_SHELL" || echo "$PATH" | grep -qc /nix/store/
-        _tide_print_item nix_shell "nix shell $tide_nix_shell_icon "
-    end
+    set -q IN_NIX_SHELL && _tide_print_item nix_shell $tide_nix_shell_icon' ' $IN_NIX_SHELL
 end
